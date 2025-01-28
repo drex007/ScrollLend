6ff77ea497a39b7d84466814c059be2ba2cda052
Pau MB <paumb85@gmail.com>
2025-01-28 23:05:07 +0100

    frontend setup

---
+++ frontend/README.md
@@ -0,0 +1,12 @@
+# ScrollLend - Frontend
+
+## Setup
+
+1. Install dependencies: 
+```bash
+npm install
+```
+2. Start development server: 
+```bash
+npm run dev
+```
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <button className="btn btn-primary">Click Me</button>
+    </>
+  );
+}
+
+export default App;
