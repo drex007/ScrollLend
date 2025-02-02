a9a964e1f9e7e7a904b7d08c0ff08e459e00cf5a
drex007 <vykeoj@gmail.com>
2025-02-02 05:28:10 +0100

    feat: added chainlink pricefeed and get asset value usd

--- contract/contracts/LendingContract.sol
+++ contract/contracts/LendingContract.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.18;
 
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
@@ -18,8 +21,9 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 // 3. User can supply to Lending Pool Contract
 
 // 4. If user health factor is below threshold, the system can liquidate the user or another user can liquidate the borrowers position 
+
 // 5. Lenders interest rate is dependent on the compound utilization ratio of each borrowed assets  i.e (Total Token borrowed/Total token supplied) 
-contract LendingBorrowingContract  is ReentrancyGuard{
+contract LendingBorrowingContract  is ReentrancyGuard, Ownable {
     //Errors
     error LendingBorrowingContract__TokenNotAllowed();
     error LendingBorrowingContract__CollateralDepositFailed();
@@ -30,7 +34,7 @@ contract LendingBorrowingContract  is ReentrancyGuard{
     //modifiers 
 
     modifier  isTokenAllowed (address token) {
-        if (s_priceFeeds[token] == address(0)){
+        if (priceFeeds[token] == address(0)){
             revert LendingBorrowingContract__TokenNotAllowed();
         }
         _;
@@ -43,20 +47,37 @@ contract LendingBorrowingContract  is ReentrancyGuard{
         _;
     }
 
+
+    constructor()Ownable(msg.sender) {
+        
+    }
+
     //Events
     event LendingBorrowingContract_DepositSuccessful(address indexed user, address indexed token, uint256 amount, uint256 timeStamp);
 
 
     //Variables
+
+    uint256 WEI_PRECISION = 1_000_000_000_000_000_000;
+    uint256 PRICE_FEED_PRECISION = 1_00_000_000;
+    address []  public collateralTokens;
+
     mapping (address user =>mapping(address token => uint256 amount)) public collateralDeposited;
-    mapping(address token => address priceFeed) public s_priceFeeds;
+    mapping(address token => address priceFeed) public priceFeeds;
+    mapping(address user => bool isActive) public debtManagementUsers;
+    mapping(address user => mapping(address  token => uint256 amount)) public liquidityPool;
+
   
 
-
+    //Owner functions 
+    function addTokenAndPriceFeed (address token, address priceFeed) public onlyOwner {
+        priceFeeds[token] = priceFeed;
+        collateralTokens.push(token);
+    }
 
     //Functions
 
-    function depositCollateral ( address  token,  uint256 amount) public isTokenAllowed(token) {
+    function depositCollateral ( address  token,  uint256 amount) public isTokenAllowed(token) isGreaterThanZero(amount) {
 
         bool success = IERC20(token).transferFrom(msg.sender, address(this), amount);
 
@@ -72,12 +93,24 @@ contract LendingBorrowingContract  is ReentrancyGuard{
         }
     }
 
-    function borrowAsset () public {
+
+    //Subscribe to automatic debt rebalancing
+    function debtManagementSubscribe () public {
+        debtManagementUsers[msg.sender] = true;
+    }
+
+    //UnSubscribe to automatic debt rebalancing
+    function debtManagementUnSubscribe () public {
+        debtManagementUsers[msg.sender] = false;
+    }
+
+
+    function borrowAsset (address token) public {
 
     }
 
 
-    function liquidatePositon () public {
+    function liquidatePositon (address token) public {
 
     }
 
@@ -88,8 +121,30 @@ contract LendingBorrowingContract  is ReentrancyGuard{
     function _getTokenUtilizationRation () public {}
 
     function _isUserPositionOpen () public {
+
     }
 
+    function _getAssetValueInUSD(address token)public view returns(uint256){
+        AggregatorV3Interface tokenPrice = AggregatorV3Interface(priceFeeds[token]);
+        (,int256 price, , ,) = tokenPrice.latestRoundData();
+        return uint256 (price )* PRICE_FEED_PRECISION;
+
+    }
+
+
+
+    function _compoundUtilizationRatio(address token) public view {
+
+    }
+
+    function _withdrawFromPool(address token, uint amount) public {
+
+    }
+
+
+    function _totalValueLocked () public {
+
+    }
 
 
 
