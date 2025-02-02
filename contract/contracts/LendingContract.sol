aae070119774e81ded4c3204f7e1bc5df6377a6b
drex007 <vykeoj@gmail.com>
2025-02-02 12:41:16 +0100

    feat: added healthcheck funcs and others

--- contract/contracts/LendingContract.sol
+++ contract/contracts/LendingContract.sol
@@ -23,11 +23,15 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/Ag
 // 4. If user health factor is below threshold, the system can liquidate the user or another user can liquidate the borrowers position 
 
 // 5. Lenders interest rate is dependent on the compound utilization ratio of each borrowed assets  i.e (Total Token borrowed/Total token supplied) 
+
 contract LendingBorrowingContract  is ReentrancyGuard, Ownable {
     //Errors
     error LendingBorrowingContract__TokenNotAllowed();
     error LendingBorrowingContract__CollateralDepositFailed();
     error LendingBorrowingContract__AmountNotMoreThanZero();
+    error LendingBorrowingContract__LiquidityDepositFailed();
+    error LendingBorrowingContract__HealthFactorIsBroken(uint256 healthFactor);
+    error LendingBorrowingContract__CollateralTokenLengthIsZero();
 
 
 
@@ -47,6 +51,13 @@ contract LendingBorrowingContract  is ReentrancyGuard, Ownable {
         _;
     }
 
+    modifier  checkForCollateralTokensLength () {
+        if (collateralTokens.length <= 0){
+            revert LendingBorrowingContract__CollateralTokenLengthIsZero();
+        }
+        _;
+    }
+
 
     constructor()Ownable(msg.sender) {
         
@@ -54,18 +65,23 @@ contract LendingBorrowingContract  is ReentrancyGuard, Ownable {
 
     //Events
     event LendingBorrowingContract_DepositSuccessful(address indexed user, address indexed token, uint256 amount, uint256 timeStamp);
+     event LendingBorrowingContract_LiquidityDepositSuccessful(address indexed user, address indexed token, uint256 amount, uint256 timeStamp);
 
 
     //Variables
-
+    uint256 HEALTH_FACTOR = 1_000_000_000_000_000_000; 
     uint256 WEI_PRECISION = 1_000_000_000_000_000_000;
-    uint256 PRICE_FEED_PRECISION = 1_00_000_000;
+    uint256 PRICE_FEED_PRECISION = 10_000_000_000;
+    uint256 BROKEN_HEALTH_FACTOR_THRESHOLD = 75_00_000_000_000_000_000; //75% of the healthfactor
     address []  public collateralTokens;
 
     mapping (address user =>mapping(address token => uint256 amount)) public collateralDeposited;
+    mapping (address user =>mapping(address token => uint256 amount)) public assetsBorrowed;
+
     mapping(address token => address priceFeed) public priceFeeds;
     mapping(address user => bool isActive) public debtManagementUsers;
     mapping(address user => mapping(address  token => uint256 amount)) public liquidityPool;
+    mapping(address token => uint256 amount) public tvl;
 
   
 
@@ -77,7 +93,7 @@ contract LendingBorrowingContract  is ReentrancyGuard, Ownable {
 
     //Functions
 
-    function depositCollateral ( address  token,  uint256 amount) public isTokenAllowed(token) isGreaterThanZero(amount) {
+    function depositCollateral ( address  token,  uint256 amount) public isTokenAllowed(token) isGreaterThanZero(amount) nonReentrant {
 
         bool success = IERC20(token).transferFrom(msg.sender, address(this), amount);
 
@@ -104,48 +120,113 @@ contract LendingBorrowingContract  is ReentrancyGuard, Ownable {
         debtManagementUsers[msg.sender] = false;
     }
 
-
-    function borrowAsset (address token) public {
-
-    }
-
-
-    function liquidatePositon (address token) public {
+    function borrowAsset (address token) public checkForCollateralTokensLength{
 
     }
 
-    function _userHealthFactor() public {
+
+    function liquidatePositon (address token, uint256 amount) public  isTokenAllowed(token) isGreaterThanZero(amount) nonReentrant {
+        bool success =  IERC20(token).transferFrom(msg.sender, address(this), amount);
+        if(success){
+
+        liquidityPool[token][msg.sender] += amount;
+        tvl[token] +=amount;
+
+        emit LendingBorrowingContract_LiquidityDepositSuccessful(msg.sender, token, amount, block.timestamp);
+
+        } else {
+            revert LendingBorrowingContract__LiquidityDepositFailed(); 
+
+        }
+
+    }
+
+    function _userHealthFactor(address user) public view  returns (uint256){
+        uint256  _usercollateralAssets = _userTotalCollateralAssetInUsd(user);
+        uint256  _userBorrowedAssets = _userTotalBorrowedAssetInUsd(user);
+
+        uint256 _heathFactor = (_usercollateralAssets * BROKEN_HEALTH_FACTOR_THRESHOLD) / _userBorrowedAssets;
+
+        return _heathFactor;
+    
         
     }
 
-    function _getTokenUtilizationRation () public {}
+    function _getTokenUtilizationRatio () public {}
 
     function _isUserPositionOpen () public {
 
     }
 
-    function _getAssetValueInUSD(address token)public view returns(uint256){
+    function _getAssetValueInUSD(address token, uint256 amount) internal view returns(uint256){
         AggregatorV3Interface tokenPrice = AggregatorV3Interface(priceFeeds[token]);
         (,int256 price, , ,) = tokenPrice.latestRoundData();
-        return uint256 (price )* PRICE_FEED_PRECISION;
+        return ((uint256 (price )* PRICE_FEED_PRECISION) * amount ) / WEI_PRECISION;
 
     }
 
 
 
-    function _compoundUtilizationRatio(address token) public view {
-
-    }
-
-    function _withdrawFromPool(address token, uint amount) public {
+    function _withdrawFromLiquidityPool(address token, uint amount) public nonReentrant {
 
     }
 
 
-    function _totalValueLocked () public {
 
+
+
+
+    function totalValueLocked () public view returns(uint256) {
+        uint256 totalCollateralValueInUsd = 0;
+        for(uint256  i = 0; i < collateralTokens.length; i++){
+            address token = collateralTokens[i];
+            uint256 amount = tvl[token];
+            totalCollateralValueInUsd += _getAssetValueInUSD(token, amount);
+
+        }
+        return totalCollateralValueInUsd;
     }
 
+
+    function _userTotalBorrowedAssetInUsd(address user) internal view returns(uint256) {
+        uint256 _userBorrowedAssets = 0;
+      
+        // Get USD value of users borrowed Assets 
+        for(uint256  i = 0; i < collateralTokens.length; i++){
+        uint256 amount = assetsBorrowed[user][collateralTokens[i]];
+        uint256 totalCollateralValueInUsd = _getAssetValueInUSD(collateralTokens[i], amount);
+        _userBorrowedAssets += totalCollateralValueInUsd;
+
+        }
+        return _userBorrowedAssets;
+        
+    }
+
+
+
+
+      // Get USD value of users Collateral Assets 
+    function _userTotalCollateralAssetInUsd(address user) internal view returns(uint256) {
+        uint256 _usercollateralAssets  = 0;
+
+        for(uint256  i = 0; i < collateralTokens.length; i++){
+            uint256 amount = collateralDeposited[user][collateralTokens[i]];
+            uint256 totalCollateralValueInUsd = _getAssetValueInUSD(collateralTokens[i], amount);
+            _usercollateralAssets  += totalCollateralValueInUsd;
+
+            }
+
+        return _usercollateralAssets;
+        
+    }
+
+    function _checkForBrokenHealthFactor(address user) internal view {
+        uint256 healthFactor = _userHealthFactor(user);
+        if(healthFactor  < HEALTH_FACTOR){
+            revert LendingBorrowingContract__HealthFactorIsBroken(healthFactor);
+        }
+
+    }
 
 
 
